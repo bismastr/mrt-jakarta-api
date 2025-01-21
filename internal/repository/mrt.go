6431f5d2baa5d1da11d20d4a55ee91065c150b4d
bismastr <bismasatrian@gmail.com>
2025-01-21 23:39:35 +0700

    add getLanes repo

--- internal/repository/mrt.go
+++ internal/repository/mrt.go
@@ -34,3 +34,51 @@ func (q *Queries) DeleteSchedule(ctx context.Context, schedule DeleteSchedule) e
 	_, err := q.db.Exec(ctx, deleteSchedule, schedule.LineID)
 	return err
 }
+
+type Station struct {
+	ID             int64
+	StationName    pgtype.Text
+	LaneID         int64
+	StationStartID int64
+	StationEndID   int64
+}
+
+const getLanes = `
+SELECT 
+	stations.id,
+	stations.name,
+	lines.id as lane_id,
+	lines.stations_id_start,
+	lines.stations_id_end
+FROM stations
+LEFT JOIN lines ON stations.id = lines.stations_id_start
+ORDER BY stations.id
+`
+
+func (q *Queries) GetLanes(ctx context.Context) ([]Station, error) {
+	rows, err := q.db.Query(ctx, getLanes)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var items []Station
+	for rows.Next() {
+		var i Station
+		if err := rows.Scan(
+			&i.ID,
+			&i.StationName,
+			&i.LaneID,
+			&i.StationStartID,
+			&i.StationEndID,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
